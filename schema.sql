892f63dac32b97d091b47c2f4587982fe918d09a
roaaabualeeqa <roaa.abualeeqa@gmail.com>
2021-04-27 14:39:14 +0300

    class08-demo

---
+++ schema.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS people;
+
+CREATE TABLE IF NOT EXISTS people (
+    first_name VARCHAR(255),
+    last_name VARCHAR(255)
+);
--- server.js
+++ server.js
@@ -5,15 +5,21 @@ const express = require('express');
 // Load Environment Variables from the .env file
 require('dotenv').config();
 const cors = require('cors');
+const pg = require('pg');
+
 
 //Application setupppp
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 8000;
 
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
+
 
 // ROUTES
 app.get('/test', testHandler);
+app.get('/add', addDataHandler);
+app.get('/people',getDataHandler);
 app.get('*', notFoundHandler); //Error Handler
 
 // Routes Handlers
@@ -22,10 +28,43 @@ function testHandler(request, response) {
 }
 
 function notFoundHandler(request, response) {
-    response.status(404).send('huh????');
+    response.status(404).send('not found !!');
 }
 
-app.listen(PORT, () =>
-    console.log(`listening on ${PORT}`)
-);
+//localhost:3010/add?first_name=roaa&last_name=AbuAleeqa
+function addDataHandler(req,res){
+    console.log(req.query);
+    let firstName = req.query.first_name;
+    let lastName = req.query.last_name;
+    //safe values
+    let SQL = `INSERT INTO people (first_name,last_name) VALUES ($1,$2) RETURNING *;`;
+    let safeValues = [firstName,lastName];
+    client.query(SQL,safeValues)
+    .then(result=>{
+        res.send(result.rows);
+    })
+    .catch(error=>{
+        res.send(error);
+    })
+}
+
+//localhost:3010/people
+function getDataHandler(req,res){
+    let SQL = `SELECT * FROM people;`;
+    client.query(SQL)
+    .then(result=>{
+        res.send(result.rows);
+    })
+    .catch(error=>{
+        res.send(error);
+    })
+}
+
+client.connect()
+    .then(() => {
+        app.listen(PORT, () =>
+        console.log(`listening on ${PORT}`)
+    );
+
+})
 
